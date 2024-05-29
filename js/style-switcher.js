2a3fddae7c7523f023d32a1351706c97c2962173
ApsaraWitharana <asachini095@gmail.com>
2024-05-29 19:12:42 +0530

    done

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -2,7 +2,7 @@
 // ===========================toggle style switcher================
 
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () => {
+styleSwitcherToggle.addEventListener("click", () =>{
     document.querySelector(".style-switcher").classList.toggle("open");
 
 })
@@ -12,9 +12,23 @@ styleSwitcherToggle.addEventListener("click", () => {
 window.addEventListener("scroll" , () => {
     if(document.querySelector(".style-switcher").classList.contains("open"))
  {
-     document.querySelector(".style-switcher").classList.remove("open");
-
-       
+     document.querySelector(".style-switcher").classList.remove("open");   
 
     }
 })
+
+//========theme colors==============
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyles.forEach((style) =>{
+        if(color === style.getAttribute("title")){
+           style.removeAttribute("disabled");
+        }
+
+        else{
+             style.setAttribute("disabled" ,"true");
+        }
+    })
+      
+}
