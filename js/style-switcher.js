f63fa5a8fcf6a94202ff2ce303af1adee2dfdc99
ApsaraWitharana <asachini095@gmail.com>
2024-05-29 18:47:24 +0530

    done

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -2,7 +2,19 @@
 // ===========================toggle style switcher================
 
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click",()=>{
+styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
 
+})
+
+// hide style -switcher on scroll
+
+window.addEventListener("scroll" , () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+ {
+     document.querySelector(".style-switcher").classList.remove("open");
+
+       
+
+    }
 })
