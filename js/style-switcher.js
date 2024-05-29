88a1d7a9eb1a50560a6907994c5eea4b4fd8d989
ApsaraWitharana <asachini095@gmail.com>
2024-05-29 19:38:27 +0530

    done

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -32,3 +32,23 @@ function setActiveStyle(color){
     })
       
 }
+
+//=================theme light and dark mode===========
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click" ,() => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+
+    if(document.body.classList.contains("dark")){
+      
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
