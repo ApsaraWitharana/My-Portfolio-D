c0345637e01da2fabaab63c8a1cc65d0335830cb
ApsaraWitharana <asachini095@gmail.com>
2024-05-29 17:55:32 +0530

    update style.switche colors-js

---
+++ js/style-switcher.js
@@ -0,0 +1,8 @@
+
+// ===========================toggle style switcher================
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click",()=>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+
+})
