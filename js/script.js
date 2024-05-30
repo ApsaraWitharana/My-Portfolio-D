fd8b8e98372407b8496b179bd664db431f4d4765
ApsaraWitharana <asachini095@gmail.com>
2024-05-30 21:12:28 +0530

    animation to script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// ===========================typing animation==========
+
+var typed = new Typed(".typing" ,{
+    strings:["Web Designer","Web Developer","Full Stack Developer"],
+    typedSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
