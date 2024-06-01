4f275a0e1aa49fefe6fb09a32ad94c37a173264c
ApsaraWitharana <asachini095@gmail.com>
2024-06-01 21:21:55 +0530

    done

--- js/script.js
+++ js/script.js
@@ -11,17 +11,40 @@ var typed = new Typed(".typing" ,{
 // ====================================Aside========================
 
 
-const nav = document.querySelector(".nav"),
-    navList = nav.querySelector("li"),
-    totalNavList = navList.clientHeight;
-    for(let i=0; i<totalNavList; i++){
-        const a = navList[i].querySelector("a");
-        a.addEventListner("click",function(){
-            for(let j=0; j<totalNavList;j++){
-                navList[j].querySelector("a").classList.remove("active");
-            
-            }
+// const nav = document.querySelector(".nav"),
+//     navList = nav.querySelector("li"),
+//     totalNavList = navList.length,
+//     allSection = document.querySelectorAll(".section"),
+//     totalSection = allSection.length;
+//     for(let i=0; i<totalNavList; i++){
 
-            this.classList.add("active")
-        })
-    }
+//         const a = navList[i].querySelector("a");
+//         a.addEventListner("click",function()
+//         {
+//             for(let i=0; i<totalSection; i++){
+//             allSection[i].classList.remove("active");
+//         }
+//             for(let j=0; j<totalNavList; j++){
+
+//                 if(navList[j].querySelector("a").classList.contains("active")){
+
+//                     allSection[j].classList.add("back-section");
+//                 }
+//                 navList[j].querySelector("a").classList.remove("active");
+            
+//             }
+
+//             this.classList.add("active")
+//             showSection(this);
+
+//         })
+//     }
+
+//     function showSection(element){
+
+//         for(let i=0; i<totalSection; i++){
+//             allSection[i].classList.remove("active");
+//         }
+//       const target =  element.getAttribute("href").split("#")[1];
+//       document.querySelector("#" + target).classList.add(active)
+//     }
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -5,6 +5,8 @@ const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () =>{
     document.querySelector(".style-switcher").classList.toggle("open");
 
+    
+
 })
 
 // hide style -switcher on scroll
