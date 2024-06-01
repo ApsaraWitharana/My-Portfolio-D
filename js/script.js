f112640d4171ab65fb4dd6061d389b36d5bd6d7b
ApsaraWitharana <asachini095@gmail.com>
2024-06-01 18:51:21 +0530

    done

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ var typed = new Typed(".typing" ,{
     BackSpeed:60,
     loop:true
 })
+
+// ====================================Aside========================
+
+
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelector("li"),
+    totalNavList = navList.clientHeight;
+    for(let i=0; i<totalNavList; i++){
+        const a = navList[i].querySelector("a");
+        a.addEventListner("click",function(){
+            for(let j=0; j<totalNavList;j++){
+                navList[j].querySelector("a").classList.remove("active");
+            
+            }
+
+            this.classList.add("active")
+        })
+    }
