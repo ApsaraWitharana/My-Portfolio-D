4b71acf7fb1eda80392ac460b9768189476c1c23
ApsaraWitharana <asachini095@gmail.com>
2024-06-05 18:31:12 +0530

    fiex js file erros

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -2,16 +2,14 @@
 // ===========================toggle style switcher================
 
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () =>{
+styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
-
-    
 
 })
 
 // hide style -switcher on scroll
 
-window.addEventListener("scroll" , () => {
+window.addEventListener("scroll", () => {
     if(document.querySelector(".style-switcher").classList.contains("open"))
  {
      document.querySelector(".style-switcher").classList.remove("open");   
