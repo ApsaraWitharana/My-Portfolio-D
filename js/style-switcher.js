5be53cbeabbf15a1413af6820a46dff1b5272275
ApsaraWitharana <asachini095@gmail.com>
2024-06-05 19:30:10 +0530

    done

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -20,9 +20,13 @@ window.addEventListener("scroll", () => {
 //========theme colors==============
 
 const alternateStyles = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color){
-    alternateStyles.forEach((style) =>{
-        if(color === style.getAttribute("title")){
+
+function setActiveStyle(color) {
+
+    alternateStyles.forEach((style) => {
+
+        if(color === style.getAttribute("title")) {
+
            style.removeAttribute("disabled");
         }
 
