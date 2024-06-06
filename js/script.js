ea354161a8d721abfd3bd007635da722275a1dab
ApsaraWitharana <asachini095@gmail.com>
2024-06-06 22:44:09 +0530

    done

--- js/script.js
+++ js/script.js
@@ -11,40 +11,40 @@ var typed = new Typed(".typing" ,{
 // ====================================Aside========================
 
 
-// const nav = document.querySelector(".nav"),
-//     navList = nav.querySelector("li"),
-//     totalNavList = navList.length,
-//     allSection = document.querySelectorAll(".section"),
-//     totalSection = allSection.length;
-//     for(let i=0; i<totalNavList; i++){
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelector("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+    for(let i=0; i<totalNavList; i++){
 
-//         const a = navList[i].querySelector("a");
-//         a.addEventListner("click",function()
-//         {
-//             for(let i=0; i<totalSection; i++){
-//             allSection[i].classList.remove("active");
-//         }
-//             for(let j=0; j<totalNavList; j++){
+        const a = navList[i].querySelector("a");
+        a.addEventListner("click",function()
+        {
+            for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("active");
+        }
+            for(let j=0; j<totalNavList; j++){
 
-//                 if(navList[j].querySelector("a").classList.contains("active")){
+                if(navList[j].querySelector("a").classList.contains("active")){
 
-//                     allSection[j].classList.add("back-section");
-//                 }
-//                 navList[j].querySelector("a").classList.remove("active");
+                    allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
             
-//             }
+            }
 
-//             this.classList.add("active")
-//             showSection(this);
+            this.classList.add("active")
+            showSection(this);
 
-//         })
-//     }
+        })
+    }
 
-//     function showSection(element){
+    function showSection(element){
 
-//         for(let i=0; i<totalSection; i++){
-//             allSection[i].classList.remove("active");
-//         }
-//       const target =  element.getAttribute("href").split("#")[1];
-//       document.querySelector("#" + target).classList.add(active)
-//     }
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("active");
+        }
+      const target =  element.getAttribute("href").split("#")[1];
+      document.querySelector("#" + target).classList.add(active)
+    }
