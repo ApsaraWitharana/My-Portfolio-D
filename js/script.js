a8a1345a74288d4272e8e065b1140f16b8e818fc
ApsaraWitharana <asachini095@gmail.com>
2024-08-13 20:26:05 +0530

    update-skill slider done

--- js/script.js
+++ js/script.js
@@ -141,3 +141,36 @@ document.getElementById('alertButton').addEventListener('click', function() {
     alert('Massage send successfully!');
 });
 
+
+//sklis//
+let slider = document.querySelector('.skills-slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; // Scroll speed
+    slider.scrollLeft = scrollLeft - walk;
+});
+
+
